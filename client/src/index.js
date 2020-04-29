d435253c07af19eb8e375f2afd974b9dad1cbcfa
limcolin <limcolin@hotmail.com>
2020-04-29 14:45:10 +0800

    Remove and unregister service worker

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
@@ -10,4 +11,5 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-registerServiceWorker();
+unregister()
+//registerServiceWorker();
